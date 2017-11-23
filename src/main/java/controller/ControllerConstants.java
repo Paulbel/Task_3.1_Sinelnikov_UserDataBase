d8b911db6f5650842106bf8cec4cf564655c7354
OSmol <Olga_Smolyakova@outlook.com>
2017-11-23 17:40:00 +0300

    Update ControllerConstants.java

--- src/main/java/controller/ControllerConstants.java
+++ src/main/java/controller/ControllerConstants.java
@@ -1,6 +1,7 @@
 package controller;
 
-public final class ControllerConstants {
+public final class ControllerConstants {// мня, ну почитай java code convention
+    // и вообще, класс - public, конструктор его privet, а поля - friendly - где мой абсент?
     static final String NAME_ATTRIBUTE = "name";
     static final String SURNAME_ATTRIBUTE = "surname";
     static final String USER_LIST_ATTRIBUTE = "userList";
